2cf0e95dd9e3debba73b99992264e7a79744b962
Euge <euge.itech@gmail.com>
2023-09-24 14:29:39 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'My Flutter App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'.toUpperCase()),
+          backgroundColor: Colors.blueGrey,
+        ),
+        body: Center(
+          child: Text.rich(
+            TextSpan(
+              text: 'My',
+              children: [
+                TextSpan(
+                    text: 'Flutter',
+                    style:
+                        TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold)),
+                TextSpan(
+                    text: 'App',
+                    style: TextStyle(fontSize: 30.0, color: Colors.blue)),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
